287646f521c9c96949ba228479c93d65c0cb3fef
Natasa Popovic <Natasa Popovic@DESKTOP-OONSV4Q>
2020-05-26 00:37:05 +0200

    BookTabKorisnik

--- src/application/BookTab.java
+++ src/application/BookTab.java
@@ -1,42 +1,73 @@
 package application;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 
 public class BookTab extends JPanel {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 7445755320045782268L;
 
-	private JPanel topPanel;
-	
+	//private JPanel topPanel;
+	GridBagConstraints gbc = new GridBagConstraints();
 	private String bookName;
 	
 	public BookTab(String text) {
 		this.bookName = text;
+		
+		
 		this.setLayout(new BorderLayout());;
 		
-		// panel za toolbar
-		this.topPanel = new JPanel();
-		topPanel.add(new JLabel("Toolbar za knjigu " + this.bookName));
-		this.add(topPanel, BorderLayout.NORTH);
+		JPanel topPanel = new JPanel();
+		JPanel centerPanel = new JPanel();
+		JPanel downPanel = new JPanel();
+		JPanel LDPanel = new JPanel();
+		JPanel RDPanel = new JPanel();
+		add(BorderLayout.PAGE_START, topPanel);
+		add(BorderLayout.PAGE_END, downPanel);
 		
-		// panel za stablo
-		JPanel leftPanel = new JPanel();
-		leftPanel.add(new JLabel("Stablo knjige " + this.bookName));
 		
-		// panel za sadrzaj stranice
-		JPanel rightPanel = new JPanel();
-		rightPanel.add(new JLabel("Sadrzaj izabrane stranice knjige " + this.bookName));
+		this.add(topPanel, BorderLayout.WEST);
+		this.add(downPanel, BorderLayout.SOUTH);
+		this.add(centerPanel, BorderLayout.CENTER);
+
+	
+		JButton btnn = new JButton("Sačuvaj");
 		
-		// split pane
-		this.add(new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, rightPanel),
-				BorderLayout.CENTER);
+	    downPanel.add(btnn);
+	  
+	    JLabel lbl = new JLabel("Sortiraj");
+	    lbl.setVisible(true);
+	   
+	    topPanel.add(lbl);
+
+	    String[] choices = { "Po imenu","Po prezimenu", "Po tipu korisnika"};
+
+	    final JComboBox<String> cb = new JComboBox<String>(choices);
+
+	    cb.setVisible(true);
+	    
+	   
+	    topPanel.add(cb);
+
+	    JButton btn = new JButton("OK");
+	    
+	   
+		
+		topPanel.add(btn);
+	   
+		
 	}
 	
 	public void saveBookState() {
