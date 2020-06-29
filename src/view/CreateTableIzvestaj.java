e991a57e4a6f2c7c31e24732e2bb42a3164b81d9
popoviccat <62185450+popoviccat@users.noreply.github.com>
2020-06-29 15:36:49 +0200

    Brisanje komentara-2

--- src/view/CreateTableIzvestaj.java
+++ src/view/CreateTableIzvestaj.java
@@ -155,7 +155,6 @@ public class CreateTableIzvestaj extends JPanel{
 		tbl = new JTable(model) {
 			public Component prepareRenderer (TableCellRenderer renderer,int Index_row, int Index_col) {
 				Component comp = super.prepareRenderer(renderer, Index_row, Index_col);
-				//even index, selected or not selected
 				if (Index_row % 2 == 0 && !isCellSelected(Index_row, Index_col)) {
 					comp.setBackground(new Color(203, 231, 218));
 					comp.setForeground(Color.black);
--- src/view/TabIzvestaj.java
+++ src/view/TabIzvestaj.java
@@ -3,7 +3,6 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -25,7 +24,6 @@ import controlers.readFromFile;
 import model.Korisnik;
 import model.Lek;
 import model.Prodaja;
-import model.Racun;
 import model.Stavka;
 import model.TipKorisnika;
 
